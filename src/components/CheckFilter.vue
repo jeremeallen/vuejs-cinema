9521a0c91c664635c22534bfef2e44fa418ce27a
Jereme Allen <jereme.allen@wheniwork.com>
2017-05-20 11:28:34 -0500

    Refactor out components

---
+++ src/components/CheckFilter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div 
+    class='check-filter' 
+    @click='checkFilter' 
+    :class='{ "check-filter": true, active: checked}'
+  >
+  <span class='checkbox'></span>
+  <span class='check-filter-title'>
+    {{ title }}
+  </span>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'check-filter',
+    props: [
+      'title',
+    ],
+    data() {
+      return {
+        checked: false,
+      }
+    },
+    methods: {
+      checkFilter() {
+        this.checked = !this.checked;
+        this.$emit('check-filter', 'genre', this.title, this.checked);
+      },
+    },
+  };
+</script>
--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -11,6 +11,7 @@
   </div>
 </template>
 <script>
+  import CheckFilter from './CheckFilter.vue';
   import genres from '../util/genres';
 
   export default {
@@ -21,37 +22,12 @@
       },
     },
     components: {
-        'check-filter': {
-            template: `<div 
-                          class='check-filter' 
-                          @click='checkFilter' 
-                          :class='{ "check-filter": true, active: checked}'
-                        >
-                        <span class='checkbox'></span>
-                        <span class='check-filter-title'>
-                          {{ title }}
-                        </span>
-                      </div>`,
-            props: [
-              'title',
-            ],
-            data() {
-              return {
-                checked: false,
-              }
-            },
-            methods: {
-              checkFilter() {
-                this.checked = !this.checked;
-                this.$emit('check-filter', 'genre', this.title, this.checked);
-              },
-            }
-        },
-      },
-      data() {
-        return {
-          genres,
-        };
-      },
+      CheckFilter,
+    },
+    data() {
+      return {
+        genres,
+      };
+    },
   };
 </script>
